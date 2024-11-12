14de89e1b0f9a539a9822de378f1dc5f7a4119f5
harsh07may <harsh07may@gmail.com>
2024-11-12 17:16:00 +0530

    new release

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import { Context } from "./index.d";
 const prisma = new PrismaClient();
 
 export const createContext = async (ctx: any): Promise<Context> => {
-  // Skip if you are not using a serverless environment
-  ctx.callbackWaitsForEmptyEventLoop = false;
+
 
   return { ...ctx, prisma };
 };
